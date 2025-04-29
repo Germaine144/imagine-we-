061af063b430c2cb2a1504ccd4697a5bebe59db1
Germaine144 <umuhiregermaine12@gmail.com>
2025-04-29 18:30:09 +0200

    making registration form and also styling

---
+++ Documents/project/react/login/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import LoginForms from './components/LoginForm';
+function App() {
+  return (
+    <div className="App">
+     <LoginForms />
+    </div>
+  );
+}
+
+export default App;
